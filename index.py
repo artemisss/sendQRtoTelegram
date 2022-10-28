2402837d465ff8425d7e689289f0605ea83fdcbc
Artem Korolev <artwow13@gmail.com>
2022-10-28 15:57:30 +0300

    add Logs

--- index.py
+++ index.py
@@ -3,7 +3,6 @@ import requests
 
 def handler(event, context):
     body = json.loads(event['body'])
-    QR = body['message']['text']
     url = "https://apipy1.herokuapp.com/QR/" + body['message']['text']
     payload = ""
     response = requests.request("POST", url, data=payload)
@@ -15,7 +14,7 @@ def handler(event, context):
         'body': json.dumps({
             'method': 'sendMessage',
             'chat_id': body['message']['chat']['id'],
-            'text':  body['message']['text']
+            'text':  "Сгенерирован код: " + body['message']['text']
         }),
         'isBase64Encoded': False
     }
