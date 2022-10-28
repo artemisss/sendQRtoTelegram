56b363a5cbf66134c52cf2202d4c21ae3260d46f
artemisss <artwow13@gmail.com>
2022-10-28 15:14:58 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,17 +1,16 @@
-import telebot
-import requests
-# Создаем экземпляр бота
-bot = telebot.TeleBot('HERE TGBot API KEY')
-# Функция, обрабатывающая команду /start
-@bot.message_handler(commands=["start"])
-def start(m, res=False):
-    bot.send_message(m.chat.id, 'Привет. В этом боте ты сможешь сгенерировать QR код. Просто отправь мне любое сообщение, а в ответ я пришлю тебе QR код.')
-# Получение сообщений от юзера
-@bot.message_handler(content_types=["text"])
-def handle_text(message):
-    bot.send_message(message.chat.id, 'Сейчас сгенерирую код: ' + message.text)
-    url = "https://secret.herokuapp.com/QR/" + message.text
-    payload = ""
-    response = requests.request("POST", url, data=payload)
-# Запускаем бота
-bot.polling(none_stop=True, interval=0)
+import json
+
+def handler(event, context):
+    body = json.loads(event['body'])
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps({
+            'method': 'sendMessage',
+            'chat_id': body['message']['chat']['id'],
+            'text':  body['message']['text']
+        }),
+        'isBase64Encoded': False
+    }
